f1f3ded2e282b009338a7be8d28679a371c3d16d
Jonathan Hood <jonathan@tangotango.net>
2021-05-20 15:53:57 -0500

    feat: add a spinlocked single thread scheduler.

---
+++ include/cask/scheduler/SingleThreadScheduler.hpp
@@ -0,0 +1,85 @@
+//          Copyright Tango Tango, Inc. 2020 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _CASK_SINGLE_THREAD_SCHEDULER_H_
+#define _CASK_SINGLE_THREAD_SCHEDULER_H_
+
+#include <atomic>
+#include <condition_variable>
+#include <map>
+#include <mutex>
+#include <thread>
+#include <queue>
+#include <vector>
+
+#include "../Scheduler.hpp"
+
+namespace cask::scheduler {
+    
+class SingleThreadScheduler final : public Scheduler, public std::enable_shared_from_this<SingleThreadScheduler> {
+public:
+    /**
+     * Construct a scheduler optionally configuring the maximum number of threads
+     * to use.
+     * 
+     * @param poolSize The number of threads to use - defaults to matching
+     *                 the number of hardware threads available in the system.
+     */
+    SingleThreadScheduler();
+
+    /**
+     * Destruct the scheduler. Destruction waits for all running and timer
+     * threads to stop before finishing.
+     */
+    ~SingleThreadScheduler();
+
+    void submit(const std::function<void()>& task) override;
+    void submitBulk(const std::vector<std::function<void()>>& tasks) override;
+    CancelableRef submitAfter(int64_t milliseconds, const std::function<void()>& task) override;
+    bool isIdle() const override;
+private:
+    using TimerEntry = std::tuple<int64_t, std::function<void()>>;
+
+    bool running;
+    bool idle;
+
+    std::condition_variable dataInQueue;
+
+    mutable std::atomic_flag readyQueueLock;
+    std::queue<std::function<void()>> readyQueue;
+    std::mutex timerMutex;
+    std::map<int64_t,std::vector<TimerEntry>> timers;
+    std::thread runThread;
+    std::thread timerThread;
+    int64_t ticks;
+    int64_t next_id;
+
+    void run();
+    void timer();
+
+    class CancelableTimer final : public Cancelable {
+    public:
+        CancelableTimer(
+            const std::shared_ptr<SingleThreadScheduler>& parent,
+            int64_t time_slot,
+            int64_t id
+        );
+
+        void cancel() override;
+        void onCancel(const std::function<void()>& callback) override;
+    private:
+        std::shared_ptr<SingleThreadScheduler> parent;
+        int64_t time_slot;
+        int64_t id;
+        std::vector<std::function<void()>> callbacks;
+        std::mutex callback_mutex;
+        bool canceled;
+    };
+
+};
+
+}
+
+#endif
---
+++ src/cask/scheduler/SingleThreadScheduler.cpp
@@ -0,0 +1,197 @@
+//          Copyright Tango Tango, Inc. 2020 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "cask/scheduler/SingleThreadScheduler.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+namespace cask::scheduler {
+
+SingleThreadScheduler::SingleThreadScheduler()
+    : running(true)
+    , idle(true)
+    , dataInQueue()
+    , readyQueueLock(ATOMIC_FLAG_INIT)
+    , readyQueue()
+    , timerMutex()
+    , timers()
+    , runThread()
+    , timerThread()
+    , ticks(0)
+{
+    runThread = std::thread(std::bind(&SingleThreadScheduler::run, this));
+    timerThread = std::thread(std::bind(&SingleThreadScheduler::timer, this));
+}
+
+SingleThreadScheduler::~SingleThreadScheduler() {
+    running = false;
+
+    try {
+        runThread.join();
+    } catch(const std::system_error& error) {}
+
+    try {
+        timerThread.join();
+    } catch(const std::system_error& error) {}
+}
+
+void SingleThreadScheduler::submit(const std::function<void()>& task) {
+    while(readyQueueLock.test_and_set());
+    readyQueue.emplace(task);
+    readyQueueLock.clear();
+}
+
+void SingleThreadScheduler::submitBulk(const std::vector<std::function<void()>>& tasks) {
+    while(readyQueueLock.test_and_set());
+    for(auto& task: tasks) {
+        readyQueue.emplace(task);
+    }
+    readyQueueLock.clear();
+}
+
+CancelableRef SingleThreadScheduler::submitAfter(int64_t milliseconds, const std::function<void()>& task) {
+    std::lock_guard<std::mutex> guard(timerMutex);
+    int64_t executionTick = ticks + milliseconds;
+
+    auto id = next_id++;
+    auto tasks = timers.find(executionTick);
+    auto entry = std::make_tuple(id, task);
+    auto cancelable = std::make_shared<CancelableTimer>(
+        this->shared_from_this(),
+        executionTick,
+        id
+    );
+
+    if(tasks == timers.end()) {
+        std::vector<TimerEntry> taskVector = {entry};
+        timers[executionTick] = taskVector;
+    } else {
+        tasks->second.push_back(entry);
+    }
+
+    return cancelable;
+}
+
+bool SingleThreadScheduler::isIdle() const {
+    while(readyQueueLock.test_and_set());
+    auto result = idle && readyQueue.empty();
+    readyQueueLock.clear();
+    return result;
+}
+
+void SingleThreadScheduler::run() {
+    std::function<void()> task;
+
+    while(running) {
+        while(readyQueueLock.test_and_set());
+        if(readyQueue.empty()) {
+            idle = true;
+            readyQueueLock.clear();
+            std::this_thread::sleep_for(0ms);
+        } else {
+            idle = false;
+            task = readyQueue.front();
+            readyQueue.pop();
+            readyQueueLock.clear();
+            task();
+        }
+    }
+}
+
+void SingleThreadScheduler::timer() {
+    const static std::chrono::milliseconds sleep_time(10);
+
+    while(running) {
+        auto before = std::chrono::high_resolution_clock::now();
+        std::this_thread::sleep_for(sleep_time);
+        auto after = std::chrono::high_resolution_clock::now();
+        
+        auto delta = after - before;
+        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(delta).count();
+
+        {
+            std::lock_guard<std::mutex> guard(timerMutex);
+
+            int64_t previous_ticks = ticks;
+            ticks += milliseconds;
+
+            for(int64_t i = previous_ticks; i <= ticks; i++) {
+                auto tasks = timers.find(i);
+                if(tasks != timers.end()) {
+                    std::vector<std::function<void()>> submittedTasks;
+                    auto entries = tasks->second;
+
+                    submittedTasks.reserve(entries.size());
+
+                    for(auto& entry : entries) {
+                        submittedTasks.emplace_back(std::get<1>(entry));
+                    }
+
+                    submitBulk(submittedTasks);
+                    timers.erase(i);
+                }
+            }
+        }
+    }
+}
+
+SingleThreadScheduler::CancelableTimer::CancelableTimer(
+    const std::shared_ptr<SingleThreadScheduler>& parent,
+    int64_t time_slot,
+    int64_t id
+)   : parent(parent)
+    , time_slot(time_slot)
+    , id(id)
+    , callbacks()
+    , callback_mutex()
+    , canceled(false)
+{}
+
+void SingleThreadScheduler::CancelableTimer::cancel() {
+    if(canceled) {
+        return;
+    } else {
+        std::lock_guard<std::mutex> parent_guard(parent->timerMutex);
+        std::lock_guard<std::mutex> self_guard(callback_mutex);
+        auto tasks = parent->timers.find(time_slot);
+        if(tasks != parent->timers.end()) {
+            std::vector<TimerEntry> filteredEntries;
+            auto entries = tasks->second;
+
+            for(auto& entry : entries) {
+                auto entry_id = std::get<0>(entry);
+                if(entry_id != id) {
+                    filteredEntries.emplace_back(entry);
+                } else {
+                    canceled = true;
+                }
+            }
+
+            if(filteredEntries.size() > 0) {
+                parent->timers[time_slot] = filteredEntries;
+            } else {
+                parent->timers.erase(time_slot);
+            }
+        }
+    }
+
+    if(canceled) {
+        for(auto& cb : callbacks) {
+            cb();
+        }
+    }
+}
+
+void SingleThreadScheduler::CancelableTimer::onCancel(const std::function<void()>& callback) {
+    if(canceled) {
+        callback();
+    } else {
+        std::lock_guard<std::mutex> guard(callback_mutex);
+        callbacks.emplace_back(callback);
+    }
+}
+
+} // namespace cask::scheduler
