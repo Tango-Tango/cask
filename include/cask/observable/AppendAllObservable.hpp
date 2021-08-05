fa89c9922d7f7f33f3b33d12ea1373a3b6950061
Jonathan Hood <jonathan@tangotango.net>
2021-08-05 12:50:55 -0500

    feat: add the appendAll operator to Observable. (#47)

---
+++ include/cask/observable/AppendAllObservable.hpp
@@ -0,0 +1,42 @@
+//          Copyright Tango Tango, Inc. 2020 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _CASK_APPEND_ALL_OBSERVABLE_
+#define _CASK_APPEND_ALL_OBSERVABLE_
+
+#include "../Observable.hpp"
+#include "../Observer.hpp"
+#include "AppendAllObserver.hpp"
+
+namespace cask::observable {
+
+template <class T, class E>
+class AppendAllObservable final : public Observable<T,E> {
+public:
+    explicit AppendAllObservable(const std::shared_ptr<const Observable<T,E>>& first, const std::shared_ptr<const Observable<T,E>>& second);
+    CancelableRef subscribe(const std::shared_ptr<Scheduler>& sched, const std::shared_ptr<Observer<T,E>>& observer) const override;
+private:
+    std::shared_ptr<const Observable<T,E>> first;
+    std::shared_ptr<const Observable<T,E>> second;
+};
+
+template <class T, class E>
+AppendAllObservable<T,E>::AppendAllObservable(const std::shared_ptr<const Observable<T,E>>& first, const std::shared_ptr<const Observable<T,E>>& second)
+    : first(first)
+    , second(second)
+{}
+
+template <class T, class E>
+CancelableRef AppendAllObservable<T,E>::subscribe(
+    const std::shared_ptr<Scheduler>& sched,
+    const std::shared_ptr<Observer<T,E>>& observer) const
+{
+    auto appendObserver = std::make_shared<AppendAllObserver<T,E>>(sched, observer, second);
+    return first->subscribe(sched, appendObserver);
+}
+
+}
+
+#endif
---
+++ test/cask/observable/TestObservableAppendAll.cpp
@@ -0,0 +1,130 @@
+//          Copyright Tango Tango, Inc. 2020 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "gtest/gtest.h"
+#include "cask/Observable.hpp"
+#include "cask/None.hpp"
+
+using cask::BufferRef;
+using cask::Observable;
+using cask::Scheduler;
+using cask::Task;
+
+TEST(ObservableAppendAll, SingleValues) {
+    auto result = Observable<int>::pure(123)
+        ->appendAll(Observable<int>::pure(456))
+        ->take(3)
+        .run(Scheduler::global())
+        ->await();
+
+    ASSERT_EQ(result.size(), 2);
+    EXPECT_EQ(result[0], 123);
+    EXPECT_EQ(result[1], 456);
+}
+
+TEST(ObservableAppendAll, UpstreamEmpty) {
+    auto result = Observable<int>::empty()
+        ->appendAll(Observable<int>::pure(456))
+        ->take(3)
+        .run(Scheduler::global())
+        ->await();
+
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], 456);
+}
+
+TEST(ObservableAppendAll, DownstreamEmpty) {
+    auto result = Observable<int>::pure(123)
+        ->appendAll(Observable<int>::empty())
+        ->take(3)
+        .run(Scheduler::global())
+        ->await();
+
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], 123);
+}
+
+TEST(ObservableAppendAll, BothEmpty) {
+    auto result = Observable<int>::empty()
+        ->appendAll(Observable<int>::empty())
+        ->take(3)
+        .run(Scheduler::global())
+        ->await();
+
+    ASSERT_EQ(result.size(), 0);
+}
+
+TEST(ObservableAppendAll, BothErrors) {
+    auto result = Observable<int, std::string>::raiseError("upstream")
+        ->appendAll(Observable<int, std::string>::raiseError("downstream"))
+        ->completed()
+        .failed()
+        .run(Scheduler::global())
+        ->await();
+
+    EXPECT_EQ(result, "upstream");
+}
+
+TEST(ObservableAppendAll, UpstreamError) {
+    auto result = Observable<int, std::string>::raiseError("upstream")
+        ->appendAll(Observable<int, std::string>::pure(456))
+        ->completed()
+        .failed()
+        .run(Scheduler::global())
+        ->await();
+
+    EXPECT_EQ(result, "upstream");
+}
+
+TEST(ObservableAppendAll, DownstreamError) {
+    auto result = Observable<int, std::string>::pure(123)
+        ->appendAll(Observable<int, std::string>::raiseError("downstream"))
+        ->completed()
+        .failed()
+        .run(Scheduler::global())
+        ->await();
+
+    EXPECT_EQ(result, "downstream");
+}
+
+TEST(ObservableAppendAll, UpstreamCancel) {
+    int counter = 0;
+
+    auto deferred = Observable<int>::fromTask(Task<int>::never())
+        ->appendAll(Observable<int>::pure(456))
+        ->foreach([&counter](auto) {
+            counter++;
+        })
+        .run(Scheduler::global());
+
+    deferred->cancel();
+
+    try {
+        deferred->await();
+        FAIL() << "Expected method to throw";
+    } catch(std::runtime_error&) {
+        EXPECT_EQ(counter, 0);
+    }
+}
+
+TEST(ObservableAppendAll, DownstreamCancel) {
+    int counter = 0;
+
+    auto deferred = Observable<int>::empty()
+        ->appendAll(Observable<int>::fromTask(Task<int>::never()))
+        ->foreach([&counter](auto) {
+            counter++;
+        })
+        .run(Scheduler::global());
+
+    deferred->cancel();
+    
+    try {
+        deferred->await();
+        FAIL() << "Expected method to throw";
+    } catch(std::runtime_error&) {
+        EXPECT_EQ(counter, 0);
+    }
+}
