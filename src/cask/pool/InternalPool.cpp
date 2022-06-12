e3b29ac71ffd18d4e8001440c28a29d11ca5f925
Jonathan Hood <jonathan@tangotango.net>
2022-06-12 18:33:07 -0500

    fix: return to using a global rather than a thread-local pool.

--- src/cask/pool/InternalPool.cpp
+++ src/cask/pool/InternalPool.cpp
@@ -1,13 +1,27 @@
 #include "cask/pool/InternalPool.hpp"
 
 std::shared_ptr<cask::Pool> cask::pool::global_pool() {
-    static thread_local std::weak_ptr<cask::Pool> pool_weak;
+    static std::atomic_flag initializing_pool = ATOMIC_FLAG_INIT;
+    static std::weak_ptr<cask::Pool> pool_weak;
 
     if (auto pool = pool_weak.lock()) {
         return pool;
     } else {
-        pool = std::make_shared<cask::Pool>();
-        pool_weak = pool;
-        return pool;
+        while(initializing_pool.test_and_set(std::memory_order_acquire)) {
+            #if defined(__cpp_lib_atomic_flag_test)
+            while (initializing_pool.test(std::memory_order_relaxed))
+            #endif
+            ;
+        }
+
+        if (auto pool = pool_weak.lock()) {
+            initializing_pool.clear(std::memory_order_release);
+            return pool;
+        } else {
+            pool = std::make_shared<cask::Pool>();
+            pool_weak = pool;
+            initializing_pool.clear(std::memory_order_release);
+            return pool;
+        }
     }
 }
