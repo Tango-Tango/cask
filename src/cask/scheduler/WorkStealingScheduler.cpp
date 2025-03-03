6de01aec2161d4b0bec6161becd6d68de79d15b9
Jonathan Hood <jonathan@thehoods.io>
2025-03-03 14:58:19 -0600

    fix: work steal from a single random thread
    Previously the work stealing algorithm would iterate all threads
    starting from a random starting point. This means that on systems
    with a lot of threads those threads would randomly wake up and
    iterate all schedulers - locking them in the process.
    
    This is precisely the kind of overhead that work stealing is
    suppossed to prevent. Instead of iterating all schedulers in
    this way - instead this changes modifies the algorithsm to
    attempt to steal from a _single_ random thread and then give
    up rather than continuing to iterate.
    
    On systems with many threads - this should help prevent a bunch
    of idle threads from stalling the whole system while they
    repeatedly wake up and try iterate all the threads.
    
    On systems with a few threads - there aren't many random choices
    to make anyway so this algorithm isn't so far off from what was
    there before. In the case of a dual-core or dual-scheduler system,
    for example, there is only one other thread to steal from anyway
    so this algorithm devolves to basically the same behavior.

--- src/cask/scheduler/WorkStealingScheduler.cpp
+++ src/cask/scheduler/WorkStealingScheduler.cpp
@@ -15,7 +15,7 @@ WorkStealingScheduler::WorkStealingScheduler(
     std::optional<std::size_t> batch_size)
     : running_thread_count(0)
 {
-    assert(poolSize > 0 && "Pool size must be greater than 0");
+    assert(poolSize > 1 && "Pool size must be greater than 1");
     auto idle_callback = std::bind(&WorkStealingScheduler::onThreadIdle, this->weak_from_this());
     auto resume_callback = std::bind(&WorkStealingScheduler::onThreadResume, this->weak_from_this());
     auto request_work_callback = std::bind(&WorkStealingScheduler::onThreadRequestWork, this->weak_from_this(), std::placeholders::_1);
@@ -26,6 +26,7 @@ WorkStealingScheduler::WorkStealingScheduler(
 
         thread_ids.push_back(thread_id);
         schedulers[thread_id] = sched;
+        thread_id_indexes[thread_id] = i;
     }
 }
 
@@ -92,45 +93,37 @@ std::vector<std::function<void()>> WorkStealingScheduler::onThreadRequestWork(co
     auto current_thread_id = std::this_thread::get_id();
 
     if (auto self = self_weak.lock()) {
-        // Select a random starting point for work stealing as a simple way to balance work
-        // and avoid lock contention
-        auto dice_roll = static_cast<std::size_t>(std::abs(std::rand()));
-        auto random_start_index = dice_roll % self->thread_ids.size();
-        auto i = random_start_index;
 
-        while (true) {
-            // Try and find a scheduler to steal work from
-            auto thread_id = self->thread_ids[i];
-
-            // Skip the current thread (the scheduler thread) to avoid attempting to
-            // steal work from the scheduler requesting the work in the first place
-            if (thread_id != current_thread_id) {
-                // Check if the scheduler exists. If it doesn't then just give up since
-                // we are probably shutting down.
-                auto scheduler_lookup = self->schedulers.find(thread_id);
-                if (scheduler_lookup == self->schedulers.end()) {
-                    break;
-                }
-
-                // Try and steal some work
-                auto batch = scheduler_lookup->second->steal(amount_requested);
-
-                // If a batch was stolen, submit it to the idle scheduler
-                if (batch.size() > 0) {
-                    return batch;
-                }
-            }
-
-            // Move to the next thread - giving up if we've already
-            // checked all threads
-            i = (i + 1) % self->thread_ids.size();
-            if (i == random_start_index) {
-                break;
-            }
+        // Attempt to lookup the current scheduler's index
+        auto current_thread_index = self->thread_id_indexes.find(current_thread_id);
+        if (current_thread_index == self->thread_id_indexes.end()) {
+            return {};
         }
-    }
 
-    return std::vector<std::function<void()>>();
+        // Select a random scheduler to steal from, adjusting so we exclude the current one
+        auto dice_roll = static_cast<std::size_t>(std::abs(std::rand()));
+        auto scheduler_index = dice_roll % (self->thread_ids.size() - 1);
+
+        if (scheduler_index >= current_thread_index->second) {
+            scheduler_index++;
+        }
+
+        auto thread_id = self->thread_ids[scheduler_index];
+
+        // Check if the scheduler exists. If it doesn't then just give up since
+        // we are probably shutting down.
+        auto scheduler_lookup = self->schedulers.find(thread_id);
+        if (scheduler_lookup == self->schedulers.end()) {
+            return {};
+        }
+
+        // Try and steal some work
+        return scheduler_lookup->second->steal(amount_requested);
+    } else {
+        // This scheduler seems to be shut down, so return an
+        // empty batch of work.
+        return {};
+    }
 }
 
 } // namespace cask::scheduler
